f5a0b013c968a06df4ea2fef4d778263146fbcad
phamviet86 <phamquangviet@bingoenglish.edu.vn>
2025-06-02 11:09:06 +0700

    add config to rootlayout

--- src/app/(front)/layout.js
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import { ConfigProvider } from "@/component/common";
-
-export default function ClientLayout({ children }) {
-  return <ConfigProvider>{children}</ConfigProvider>;
-}
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,21 @@
-import { AntdRegistry } from "@ant-design/nextjs-registry";
-import "antd/dist/reset.css";
+"use client";
 
-export const metadata = {
-  title: "Bingo English",
-  description: "Bingo learning management system",
-};
+import "antd/dist/reset.css";
+import { AntdRegistry } from "@ant-design/nextjs-registry";
+import { ConfigProvider } from "@/component/common";
+import Head from "next/head";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <title>Bingo English</title>
+        <meta name="description" content="Bingo learning management system" />
+      </Head>
       <body>
-        <AntdRegistry>{children}</AntdRegistry>
+        <AntdRegistry>
+          <ConfigProvider>{children}</ConfigProvider>
+        </AntdRegistry>
       </body>
     </html>
   );
